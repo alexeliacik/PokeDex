e9e8302418e530afbf5fbb01ebea88c3f898dfea
alexeliacik <alexeliacik@gmail.com>
2020-04-11 21:57:05 -0400

    Css arrangment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React, { useState, useEffect } from "react"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import Header from "./components/Header/Header"
 import Pokemons from "./components/Pokemons/Pokemons"
 import Pokemon from "./components/Pokemon/Pokemon"
 import Error from "./components/UI/Error/Error"
-import styled, { createGlobalStyle } from "styled-components"
+import StyledApp, { GlobalStyle } from "./StyledApp"
+
 import axios from "axios"
 import PropTypes from "prop-types"
 
-const GlobalStyle = createGlobalStyle`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-`
-const StyledApp = styled.div`
-  max-width: 600px;
-  min-height: 100vh;
-  margin: 0 auto;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  text-align: center;
-  padding: 20px 0;
-  box-shadow: 0px 3px 10px rgba(56, 56, 56, 0.3);
-`
 const App = () => {
   const [pokemons, setPokemons] = useState([])
   const [error, setError] = useState(false)
